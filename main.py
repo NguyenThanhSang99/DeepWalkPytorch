8413cef52f8dc4982dd41ae6e41d48016953c64a
NguyenThanhSang99 <50515431+NguyenThanhSang99@users.noreply.github.com>
2022-09-21 00:21:34 +0900

    Make an simple DeepWalk model

--- main.py
+++ main.py
@@ -1,38 +1,89 @@
-import importlib.util
-import sys
+#### Import necessary packages ####
+import torch
+import torch.nn as nn
 import random
-from word2vecmodel import Word2VecModel
-import graph
 
+def read_data(path):
+    f = open(path, 'r', encoding='utf8')
+    lines = f.readlines()
+    data = [[ int(node) for node in line.rstrip().split(",")] for line in lines if line.rstrip() != '']
+    return data
+
+#### Random Walk ####
+def RandomWalk(adj_list, started_node, walk_length):
+    walk = [started_node]        # Walk starts from this node
+    node = started_node
+    
+    for i in range(walk_length-1):
+        random_node = random.randint(0,len(adj_list[node])-1)
+        node = adj_list[node][random_node]
+        walk.append(node)
+
+    return walk
+
+
+class Model(torch.nn.Module):
+    def __init__(self, size_vertex, embedding_size):
+        super(Model, self).__init__()
+        self.phi  = nn.Parameter(torch.rand((size_vertex, embedding_size), requires_grad=True))    
+        self.phi2 = nn.Parameter(torch.rand((embedding_size, size_vertex), requires_grad=True))
+        
+        
+    def forward(self, one_hot):
+        hidden = torch.matmul(one_hot, self.phi)
+        out    = torch.matmul(hidden, self.phi2)
+        return out
+
+
+def skip_gram(model, size_vertex, walk,  window_size, learning_rate):
+    for j in range(len(walk)):
+        for k in range(max(0,j-window_size) , min(j+window_size, len(walk))):
+            #generate one hot vector
+            one_hot          = torch.zeros(size_vertex)
+            one_hot[walk[j]]  = 1
+            
+            out              = model(one_hot)
+            loss             = torch.log(torch.sum(torch.exp(out))) - out[walk[k]]
+            loss.backward()
+            
+            for param in model.parameters():
+                param.data.sub_(learning_rate*param.grad)
+                param.grad.data.zero_()
+
+def get_vertex_labels(graph):
+    labels = []
+    for nodes in graph:
+        labels = labels + [node for node in nodes if not node in labels]
+    return labels
+
+def DeepWalk(data, embedding_size, window_size, number_walks, walk_length, learning_rate):
+    size_vertex = len(data)  # number of vertices
+    vertex_labels = get_vertex_labels(data)
+    model = Model(size_vertex=size_vertex, embedding_size=embedding_size)
+    for i in range(number_walks):
+        print("Walk step {}".format(i + 1))
+        random.shuffle(vertex_labels)
+        for vertex in vertex_labels:
+            walk = RandomWalk(data, vertex, walk_length)
+            skip_gram(model, size_vertex, walk, window_size, learning_rate)
+
+    return model
 
 def main():
-    number_walks = 8
-    walk_length = 10
-    data_path = "data/data.edgelist"
+    path = "data/nodes.csv"
+    data = read_data(path)
+    print(data)
 
-    G = graph.load_data(data_path)
+    #### Hyperparameters ####
+    window_size  = 3            # window size
+    embedding_size  = 5         # embedding size
+    number_walks = 200          # walks per vertex
+    walk_length  = 6            # walk length
+    learning_rate = 0.025       # learning rate
 
-    print("Number of nodes: {}".format(len(G.nodes())))
+    model = DeepWalk(data, embedding_size, window_size, number_walks, walk_length, learning_rate)
 
-    total_num_walks = len(G.nodes()) * number_walks
-
-    data_size = number_walks * walk_length
-
-
-    print("Walking...")
-    walks_graph = graph.build_deepwalk_corpus(G, num_paths=number_walks,
-                                      path_length=walk_length, alpha=0, rand=random.Random(0))
-
-    representation_size = 128
-    window_size = 10
-
-    model = Word2VecModel(walks_graph, size=representation_size, window=window_size, iter=10, min_count=0, sg=1, hs=1, workers=4, compute_loss=True)
-
-    output_model = "data/data.embeddings"
-
-    print(model.idx2vec)
-
-    model.save_emb(output_model, G.nodes())
+    print(model.phi)
 
 if __name__ == '__main__':
     main()
